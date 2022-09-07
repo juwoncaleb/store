2ac072c9cd565fbae674f218d49934ee755ebbe7
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2022-09-07 05:27:20 -0700

    new update

--- pages/auth.js
+++ pages/auth.js
@@ -1,13 +1,47 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
-// AUTHENTICATION
-import { useSession, signIn, signOut } from "next-auth/react"
 export default function auth() {
-    const { data: session } = useSession()
+    const router = useRouter()
+
     return (
-        <div>
-            <button className="text-7xl headertext">GOOGLE</button>
-{if}
+        <div className='flex jusify-between'>
+            {/* FORM */}
+
+            <div>
+                <div>
+                    <img onClick={() => router.push('/')} className="lacoste cursor-pointer" src="./logos.png" />
+
+                </div>
+
+                <p className=' text-4xl font-medium headerText '>Welcome to Lacoste </p>
+                <p className='smallerText text-1xl font-light  '>Please enter your account details</p>
+                <button className='loginButton'>
+                    <img onClick={() => router.push('/')} className="lacoste cursor-pointer" src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png" />
+                    </button>
+                    <br/>
+                {/* <input placeholder='Email' type='text' />
+                <input placeholder='Password' type='password' /> */}
+
+                <div>
+                    <div>
+                        <input type="checkbox" />
+                        <p>Remember on this device</p>
+                    </div>
+
+                    <p>Forgot password ?</p>
+                </div>
+
+                <button type="button"> Login</button>
+                <p>Dont have an account ? Sign up for free</p>
+            </div>
+
+            {/* IMAGE SECTION */}
+            <div>
+                <img onClick={() => router.push('/')} className="lacoste cursor-pointer" src="./trevv.png" />
+                <p>Dress like you’re already famous</p>
+                <p>Simply elegant.</p>
+            </div>
         </div>
     )
 }
---
+++ pages/createAccount.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function createAccount() {
+  return (
+    <div>createAccount</div>
+  )
+}
