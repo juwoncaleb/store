07f3e56ce7b4addb86c73d85ef7f5d5472784ca7
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2022-09-10 07:39:52 -0700

    quick fix

--- pages/checkoutThree.js
+++ pages/checkoutThree.js
@@ -1,7 +1,181 @@
 import React from 'react'
-
+import Header from "./components/header";
+import Footer from "./components/Footer";
 export default function checkoutThree() {
   return (
-    <div>checkoutThree</div>
+    <div>
+      <Header />
+      <p className="checkoutTwo_Text text-6xl "> <span className="GreyText" >1</span ><span className="pl-5">-</span><span className='GreyText pl-4'>2</span><span className="pl-5">-</span><span className=' pl-4'>3</span></p>
+      <p className='shipping_Header text-7xl mb-20  '>PAYMENT</p>
+
+      <div className=" flex justify-between">
+        <div>
+          <div>
+            <p className='checkoutHeader text-left ml-40' >GROUND - FREE</p>
+
+
+            <div className='finalcheckout_details flex justify-center mr-40 pl-20 mt-4'>
+              <input type='checkbox' />
+              <p className="text-justify ml-10 smallerText">Based on national delays with the shipping providers , packages arrives within 5
+                to 10 working days <br />from when it is shipped . Due to precautionary measures  at the warehouse
+              </p>
+            </div>
+            <p className='checkoutHeader text-left ml-40 mt-10' >EXPRESS DELIVERY - $20</p>
+
+
+            <div className='finalcheckout_details flex justify-center mr-40 pl-20 mt-4'>
+              <input type='checkbox' />
+              <p className="text-justify ml-10 smallerText ">Based on national delays with the shipping providers , packages arrives within 5
+                to 10 working days <br />from when it is shipped . Due to precautionary measures  at the warehouse
+              </p>
+            </div>
+
+
+
+          </div>
+
+          <div>
+            <p className='checkoutHeader text-left ml-40 mt-10'>SHIPPING ADDRESS (*required)</p>
+            <p className="text-justify ml-40 smallerText mt-4 ">Items will be deliverd directly to your doorstop , based on the given address</p>
+            <div className=' mt-8 smallerText font-bold ml-20 flex jusify-around pl-20'>
+
+              <div>
+                <p>Title </p>
+              </div>
+
+              <div className='flex jusify-around ml-20'>
+                <input type="checkbox" />
+                <p className="ml-2">Mr</p>
+              </div>
+
+              <div className='flex jusify-around ml-20'>
+                <input type="checkbox" />
+                <p className="ml-2">Ms</p>
+              </div>
+
+              <div className='flex jusify-around ml-20'>
+                <input type="checkbox" />
+                <p className="ml-2">Mrs</p>
+              </div>
+
+            </div>
+          </div>
+
+
+
+          {/* FINAL CART */}
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        <div>
+          <hr className='cart' />
+          <div className='flex justify-around mt-6 mb-6'>
+            <div className="basis-1/4">
+              <img className='cartImg ml-20' src="./dd.png" />
+
+            </div>
+            <div className="mt-8 basis-1/2">
+              <div className='ml-40'>
+                <div>
+                  <div className='checkoutHeader flex justify-start'>
+                    <p className='text-2xl'>Limted Multicolor Sweater</p>
+
+                  </div>
+                </div>
+                <div className="flex justify-start mt-8">
+                  <p className="smallerText font-semibold	">Color</p>
+                  <div>
+                    <div className='color flex justify-center ml-10'></div>
+                  </div>
+                  <p className="ml-20 smallerText font-semibold	count">- 1 +</p>
+                </div>
+                <div className="flex justify-start mt-4">
+                  <p className='smallerText font-semibold'>size : XXL</p>
+                </div>
+
+              </div>
+            </div>
+
+
+            <div className='basis-1/4'>
+              <p className='smallerText font-semibold text-3xl'>$100</p>
+
+            </div>
+
+          </div>
+          <div className='flex justify-around mt-6 mb-6'>
+            <div className="basis-1/4">
+              <img className='cartImg ml-20' src="./dd.png" />
+
+            </div>
+            <div className="mt-8 basis-1/2">
+              <div className='ml-40'>
+                <div>
+                  <div className='checkoutHeader flex justify-start'>
+                    <p className='text-2xl'>Limted Multicolor Sweater</p>
+
+                  </div>
+                </div>
+                <div className="flex justify-start mt-8">
+                  <p className="smallerText font-semibold	">Color</p>
+                  <div>
+                    <div className='color flex justify-center ml-10'></div>
+                  </div>
+                  <p className="ml-20 smallerText font-semibold	count">- 1 +</p>
+                </div>
+                <div className="flex justify-start mt-4">
+                  <p className='smallerText font-semibold'>size : XXL</p>
+                </div>
+
+              </div>
+            </div>
+
+
+            <div className='basis-1/4'>
+              <p className='smallerText font-semibold text-3xl'>$100</p>
+
+            </div>
+
+          </div>
+          <hr className='cart' />
+          <div className=' mr-20'>
+            <div className='flex justify-between final_checkout_Header'>
+              <p>2 PRODUCTS</p>
+              $ 200
+            </div>
+            <div className='flex justify-between smallerText font-semibold mt-4'>
+              <p>Shipping</p>
+              $ 10
+            </div>
+            <div className='flex justify-between smallerText font-semibold mt-4'>
+              <p>VAT</p>
+              $ 13
+            </div>
+            <hr className='checkout_Line' />
+            <div className='flex justify-between smallerText font-semibold mt-4'>
+              <p>TOTAL</p>
+              <p>$350</p>
+            </div>
+
+            <div className='proceed_checkOut'>
+              <p>Proceed to checkout</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   )
 }
--- pages/checkoutTwo.js
+++ pages/checkoutTwo.js
@@ -6,19 +6,19 @@ export default function checkoutTwo() {
     <div>
       <Header />
       <p className="checkoutTwo_Text text-6xl "> <span className="GreyText" >1</span ><span className="pl-5">-</span><span className=' pl-4'>2</span><span className="pl-5">-</span><span className='GreyText pl-4'>3</span></p>
-      <p className='shipping_Header text-6xl '>Shipping</p>
+      <p className='shipping_Header text-7xl '>Shipping</p>
       <p className='shipping_subHeader  '>ENTER YOUR EMAIL ADDRESS TO <br /> CONTINUE</p>
-      <p>    Email will be used to track your order . If you dont have an account <br /> you will be able to create one during the shipping process </p>
+      <p className='shipping_text mt-4'>    Email will be used to track your order . If you dont have an account <br /> you will be able to create one during the shipping process </p>
 
       <div className='email_Input'>
         <div>
-          <input className='email_Input_bar' />
+          <input placeholder='Your email address' className='email_Input_bar' />
         </div>
         <div className='button_email'>
-          <p>hkjlk</p>
+          <p className='shipping_text '>Continue</p>
         </div>
       </div>
-      <p className="mt-10 ">Sign up with your socials</p>
+      <p className='shipping_text mt-10'>Sign up with your socials</p>
       <div className='flex justify-center mt-4'>
         <img className='mr-4 signUpIcon' src="https://img.icons8.com/ios-filled/50/000000/twitter.png" />
         <img className='signUpIcon' src="https://img.icons8.com/color/48/000000/google-logo.png" />
