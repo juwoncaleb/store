77eb451649534361cb9c17158c414efe998b7890
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2022-10-17 14:44:47 +0100

    fixed the cart

--- pages/summerFine.js
+++ pages/summerFine.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from '../component/Footer'
 import Header from '../component/Header'
+import Link from "next/link"
 
 
 export default function summerFine({ allProducts }) {
@@ -12,14 +13,15 @@ export default function summerFine({ allProducts }) {
             <div class="grid productSection lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4">
                 {
                     allProducts.map((summerFineItem) => (
-                        <div className='productGrid' key={summerFineItem._id}>
-                            <img className='productImages' src={summerFineItem.images}/>
-                            <br/>
-                            {summerFineItem.name}
-                            <br/>
-                            {summerFineItem.price}
-                        
-                        </div>
+                        <Link href={`/summerFineItem/${summerFineItem._id}`} passHref>
+                            <div className='productGrid' key={summerFineItem._id}>
+                                <img className='productImages' src={summerFineItem.images} />
+                                <br />
+                                {summerFineItem.name}
+                                <br />
+                                {summerFineItem.price}
+                            </div>
+                        </Link>
                     ))
                 }
             </div>
@@ -29,7 +31,7 @@ export default function summerFine({ allProducts }) {
 }
 
 export async function getServerSideProps() {
-    const prodRes = await fetch("http://localhost:3000/api/summerFine")
+    const prodRes = await fetch("http://localhost:3000/api/products")
     const data = await prodRes.json()
     console.log(data);
     return {
